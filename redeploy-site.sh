c04245e2217d44f3a8446241735f794af7a7bc31
Krishay Garg <76239938+krishaygarg@users.noreply.github.com>
2026-07-12 14:52:44 -0700

    Update redeploy-site.sh to use systemd instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ssh root@138.68.229.206 << 'EOF'
-tmux kill-server 2>/dev/null || true
 cd /root/MLH_personal_portfolio
 
 git fetch && git reset origin/main --hard
@@ -9,5 +8,6 @@ git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s flask_server "cd /root/MLH_personal_portfolio && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+systemctl restart myportfolio
 EOF
+
